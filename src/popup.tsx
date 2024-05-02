7e8d84191119c8a2978eb51b07a6bd1ef4e34ee6
dataraj <dhadas808@yahoo.com>
2024-05-03 00:31:44 +0530

    changed the loader file

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,14 @@
+import PlasmoOverlay from "~content"
 import ButtonComponent from "~features/count-button"
 import LogoVisibleBtn from "~features/logoVisibleBtn"
+import MessagePrompt from "~features/promptMessage"
 
 import "~style.css"
 
 function IndexPopup() {
   return (
     <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <ButtonComponent />
+      <PlasmoOverlay />
     </div>
   )
 }
